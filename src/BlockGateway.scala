a8d0b4454f9a82b6d5f362808af01a3c09707094
target-san <target.san@gmail.com>
2014-03-28 00:24:08 +0200

    Gateway block sketch

---
+++ src/BlockGateway.scala
@@ -0,0 +1,8 @@
+package targetsan.mcmods.gateway
+
+import net.minecraft.block.Block
+import net.minecraft.block.material.Material
+
+class BlockGateway(blockId: Int) extends Block(blockId, Material.rock) {
+
+}
--- src/ModMain.scala
+++ src/ModMain.scala
@@ -1,8 +1,6 @@
 package targetsan.mcmods.gateway
 
-import cpw.mods.fml.common.event.FMLPreInitializationEvent
-import cpw.mods.fml.common.event.FMLInitializationEvent
-import cpw.mods.fml.common.event.FMLPostInitializationEvent
+import cpw.mods.fml.common.event._
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.network.NetworkMod
 
