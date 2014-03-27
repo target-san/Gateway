7cec2b5b6bb921ce83d81fb3db109b1807eb81b5
Igor Baidiuk <ibaid@softserveinc.com>
2014-03-27 19:27:51 +0200

    Sketch for mod main

--- src/ModMain.scala
+++ src/ModMain.scala
@@ -1,1 +1,18 @@
 package targetsan.mcmods.gateway
+
+import cpw.mods.fml.common.event.FMLPreInitializationEvent
+import cpw.mods.fml.common.event.FMLInitializationEvent
+import cpw.mods.fml.common.event.FMLPostInitializationEvent
+import cpw.mods.fml.common.Mod
+import cpw.mods.fml.common.network.NetworkMod
+
+@Mod(modid = "Gateway", useMetadata = true, modLanguage = "scala")
+@NetworkMod(clientSideRequired = true, serverSideRequired = true)
+object ModMain {
+    @Mod.EventHandler
+	def preInit(event: FMLPreInitializationEvent) { }	
+    @Mod.EventHandler
+	def init(event: FMLInitializationEvent) { }
+    @Mod.EventHandler
+	def postInit(event: FMLPostInitializationEvent) { }
+}
