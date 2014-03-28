c3a8970b4b7c63f82f9df710f05729c4c2d02a2e
Igor Baidiuk <ibaid@softserveinc.com>
2014-03-28 12:13:44 +0200

    Added Gateway block to game. Problems with texture

--- src/targetsan/mcmods/gateway/BlockGateway.scala
+++ src/targetsan/mcmods/gateway/BlockGateway.scala
@@ -4,6 +4,7 @@ import net.minecraft.block.{Block, BlockObsidian}
 import net.minecraft.block.material.Material
 import net.minecraft.entity.Entity
 import net.minecraft.world.World
+import net.minecraft.creativetab.CreativeTabs
 
 class BlockGateway(blockId: Int) extends BlockObsidian(blockId) {
 	setHardness(50.0F)
@@ -11,6 +12,7 @@ class BlockGateway(blockId: Int) extends BlockObsidian(blockId) {
     setStepSound(Block.soundStoneFootstep)
     setUnlocalizedName("gateway")
     setTextureName("gateway")
+    setCreativeTab(CreativeTabs.tabRedstone)
     
     override def onEntityWalking(world: World, x: Int, y: Int, z: Int, entity: Entity) {
 	    
--- src/targetsan/mcmods/gateway/ModMain.scala
+++ src/targetsan/mcmods/gateway/ModMain.scala
@@ -3,14 +3,34 @@ package targetsan.mcmods.gateway
 import cpw.mods.fml.common.event._
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.network.NetworkMod
+import net.minecraftforge.common.Configuration
+import java.io.File
+import cpw.mods.fml.common.registry.GameRegistry
+import cpw.mods.fml.common.registry.LanguageRegistry
 
 @Mod(modid = "Gateway", useMetadata = true, modLanguage = "scala")
 @NetworkMod(clientSideRequired = true, serverSideRequired = true)
 object ModMain {
+    val BLOCK_GATEWAY_ID = 1024
+    private var gatewayId: Int = 0
+    var blockGateway: BlockGateway = null
+    
     @Mod.EventHandler
-	def preInit(event: FMLPreInitializationEvent) { }	
+	def preInit(event: FMLPreInitializationEvent) {
+        val configFile = new File(event.getModConfigurationDirectory().getAbsolutePath() + "/Gateway.cfg")
+        val config = new Configuration(configFile)
+        config.load()
+        
+        gatewayId = config.getBlock("gateway", BLOCK_GATEWAY_ID).getInt()
+        
+        config.save()
+    }
     @Mod.EventHandler
-	def init(event: FMLInitializationEvent) { }
+	def init(event: FMLInitializationEvent) {
+        blockGateway = new BlockGateway(gatewayId)
+        GameRegistry.registerBlock(blockGateway, "gateway")
+        LanguageRegistry.addName(blockGateway, "Gateway")
+    }
     @Mod.EventHandler
 	def postInit(event: FMLPostInitializationEvent) { }
 }
