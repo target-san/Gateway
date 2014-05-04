d9cc04c2c675069beb012fc5231c11d60f589005
target-san <target.san@gmail.com>
2014-05-04 23:29:11 +0300

    Semi-fixed player backflip glitches.
    An attempt to mark ridden cart as dead results in that entity being silently deleted from loaded entities list before entities update tick ends. This goes to IndexOutOfBoundsException. The strange thing is that non-ridden entities work just fine.

--- src/targetsan/mcmods/gateway/Teleporter.java
+++ src/targetsan/mcmods/gateway/Teleporter.java
@@ -20,6 +20,7 @@ import net.minecraft.potion.PotionEffect;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ServerConfigurationManager;
 import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.Chunk;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Teleporter {
@@ -30,7 +31,7 @@ public class Teleporter {
 		return transferEntityWithRider(entity, x, y, z, destination);
 	}
 
-	static Entity transferEntityWithRider(Entity entity, double x, double y, double z, WorldServer world) {
+	private static Entity transferEntityWithRider(Entity entity, double x, double y, double z, WorldServer world) {
 		Entity rider = entity.riddenByEntity;
 
 		if (rider != null) {
@@ -47,51 +48,58 @@ public class Teleporter {
 		return entity;
 	}
 
-	static Entity transferEntity(Entity entity, double x, double y, double z, WorldServer world) {
+	private static Entity transferEntity(Entity entity, double x, double y, double z, WorldServer world) {
 		return entity.worldObj.provider.dimensionId == world.provider.dimensionId
 			? transferEntityWithinDimension(entity, x, y, z)
 			: transferEntityToDimension(entity, x, y, z, (WorldServer) entity.worldObj, world);
 	}
+	
+	private static NBTTagCompound removeFromWorld(Entity entity) {
+		NBTTagCompound tag = new NBTTagCompound();
+		entity.writeToNBTOptional(tag);
+		/*
+		int chunkX = entity.chunkCoordX;
+		int chunkZ = entity.chunkCoordZ;
+		WorldServer world = (WorldServer)entity.worldObj;
 
-	static Entity transferEntityToDimension(Entity entity, double x, double y, double z, WorldServer exitingWorld, WorldServer enteringWorld) {
+		if (entity.addedToChunk && world.getChunkProvider().chunkExists(chunkX, chunkZ)) {
+			Chunk chunk = world.getChunkFromChunkCoords(chunkX, chunkZ);
+			chunk.removeEntity(entity);
+			chunk.isModified = true;
+		}
+
+		world.loadedEntityList.remove(entity);
+		world.onEntityRemoved(entity);
+		*/
+		entity.isDead = true;
+		
+		return tag;
+	}
+
+	private static Entity transferEntityToDimension(Entity entity, double x, double y, double z, WorldServer exitingWorld, WorldServer enteringWorld) {
 		if (entity == null)
 			return null;
 
 		if (entity instanceof EntityPlayer)
 			return transferPlayerToDimension((EntityPlayerMP)entity, x, y, z, exitingWorld, enteringWorld);
 
-		NBTTagCompound tag = new NBTTagCompound();
-		entity.writeToNBTOptional(tag);
+		NBTTagCompound tag = removeFromWorld(entity);
+		entity = EntityList.createEntityFromNBT(tag, enteringWorld);
 
-		int chunkX = entity.chunkCoordX;
-		int chunkZ = entity.chunkCoordZ;
-
-		if (entity.addedToChunk && exitingWorld.getChunkProvider().chunkExists(chunkX,
-						chunkZ)) {
-			exitingWorld.getChunkFromChunkCoords(chunkX, chunkZ)
-					.removeEntity(entity);
-		}
-
-		exitingWorld.loadedEntityList.remove(entity);
-		exitingWorld.onEntityRemoved(entity);
-
-		Entity newEntity = EntityList.createEntityFromNBT(tag,
-				enteringWorld);
-
-		if (newEntity != null) {
-			newEntity.setLocationAndAngles(x, y, z, entity.rotationYaw, entity.rotationPitch);
-			newEntity.forceSpawn = true;
-			enteringWorld.spawnEntityInWorld(newEntity);
-			newEntity.setWorld(enteringWorld);
+		if (entity != null) {
+			setLocation(entity, x, y, z);
+			entity.forceSpawn = true;
+			enteringWorld.spawnEntityInWorld(entity);
+			entity.setWorld(enteringWorld);
 		}
 
 		exitingWorld.resetUpdateEntityTick();
 		enteringWorld.resetUpdateEntityTick();
 
-		return newEntity;
+		return entity;
 	}
 
-	static Entity transferPlayerToDimension(EntityPlayerMP player, double x, double y, double z, WorldServer exitingWorld, WorldServer enteringWorld) {
+	private static Entity transferPlayerToDimension(EntityPlayerMP player, double x, double y, double z, WorldServer exitingWorld, WorldServer enteringWorld) {
 		MinecraftServer server = player.mcServer;
 		ServerConfigurationManager config = server.getConfigurationManager();
 
@@ -106,24 +114,25 @@ public class Teleporter {
 
 		exitingWorld.removePlayerEntityDangerously(player);
 		player.isDead = false;
-		player.setLocationAndAngles(x, y, z, player.rotationYaw, player.rotationPitch);
+		exitingWorld.getPlayerManager().removePlayer(player);
+		setLocation(player, x, y, z);
 
+		enteringWorld.getPlayerManager().addPlayer(player);
 		enteringWorld.spawnEntityInWorld(player);
 		player.setWorld(enteringWorld);
 
-		config.func_72375_a(player, exitingWorld);
 		player.playerNetServerHandler.setPlayerLocation(x, y, z, player.rotationYaw, player.rotationPitch);
 		player.theItemInWorldManager.setWorld(enteringWorld);
 
 		config.updateTimeAndWeatherForPlayer(player, enteringWorld);
 		config.syncPlayerInventory(player);
 
-		Iterator potion = player.getActivePotionEffects().iterator();
-		while (potion.hasNext()) {
-			player.playerNetServerHandler
-					.sendPacketToPlayer(new Packet41EntityEffect(
-							player.entityId, (PotionEffect) potion.next()));
-		}
+		for (Object potion: player.getActivePotionEffects())
+			player
+				.playerNetServerHandler
+				.sendPacketToPlayer(
+					new Packet41EntityEffect(player.entityId, (PotionEffect) potion)
+				);
 
 		player.playerNetServerHandler
 				.sendPacketToPlayer(new Packet43Experience(
@@ -134,41 +143,36 @@ public class Teleporter {
 		return player;
 	}
 	
-	static Entity transferEntityWithinDimension(Entity entity, double x, double y, double z) {
+	private static void setLocation(Entity entity, double x, double y, double z) {
+		entity.setLocationAndAngles(x, y, z, entity.rotationYaw, entity.rotationPitch);
+	}
+	
+	private static Entity transferEntityWithinDimension(Entity entity, double x, double y, double z) {
 		if (entity == null)
 			return null;
 
-		if (entity instanceof EntityPlayer) {
-			EntityPlayerMP player = (EntityPlayerMP) entity;
-			player.setPositionAndUpdate(x, y, z);
-			player.worldObj.updateEntityWithOptionalForce(player, false);
-			return player;
-		}
+		if (entity instanceof EntityPlayer)
+			return transferPlayerWithinDimension((EntityPlayerMP)entity, x, y, z);
 
 		WorldServer world = (WorldServer) entity.worldObj;
-		NBTTagCompound tag = new NBTTagCompound();
-		entity.writeToNBTOptional(tag);
 
-		int chunkX = entity.chunkCoordX;
-		int chunkZ = entity.chunkCoordZ;
+		NBTTagCompound tag = removeFromWorld(entity);
+		entity = EntityList.createEntityFromNBT(tag, world);
 
-		if (entity.addedToChunk && world.getChunkProvider().chunkExists(chunkX, chunkZ))
-			world.getChunkFromChunkCoords(chunkX, chunkZ).removeEntity(entity);
-
-		world.loadedEntityList.remove(entity);
-		world.onEntityRemoved(entity);
-
-		Entity newEntity = EntityList.createEntityFromNBT(tag, world);
-
-		if (newEntity != null) {
-			newEntity.setLocationAndAngles(x, y, z, entity.rotationYaw, entity.rotationPitch);
-			newEntity.forceSpawn = true;
-			world.spawnEntityInWorld(newEntity);
-			newEntity.setWorld(world);
+		if (entity != null) {
+			setLocation(entity, x, y, z);
+			entity.forceSpawn = true;
+			world.spawnEntityInWorld(entity);
+			entity.setWorld(world);
 		}
 
 		world.resetUpdateEntityTick();
-		return newEntity;
+		return entity;
 	}
-
+	
+	private static Entity transferPlayerWithinDimension(EntityPlayerMP player, double x, double y, double z) {
+		player.setPositionAndUpdate(x, y, z);
+		player.worldObj.updateEntityWithOptionalForce(player, false);
+		return player;
+	}
 }
