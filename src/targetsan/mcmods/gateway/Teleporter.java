597a19b502aa1c7557df153e168cda6c5ec2c7e7
target-san <target.san@gmail.com>
2014-05-08 01:41:01 +0300

    Tried to use algorithm which is based on original Minecraft's one for moving between dimension. Still no luck though.

--- src/targetsan/mcmods/gateway/Teleporter.java
+++ src/targetsan/mcmods/gateway/Teleporter.java
@@ -10,7 +10,6 @@
 package targetsan.mcmods.gateway;
 
 import java.util.Arrays;
-import java.util.Iterator;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
@@ -24,7 +23,6 @@ import net.minecraft.potion.PotionEffect;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ServerConfigurationManager;
 import net.minecraft.world.WorldServer;
-import net.minecraft.world.chunk.Chunk;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class Teleporter {
@@ -52,9 +50,9 @@ public class Teleporter {
 
 		if (rider != null) {
 			rider = transferEntityWithRider(rider, x, y, z, world);
-			rider.mountEntity(entity);
 			if (rider instanceof EntityPlayer)
 				rider.worldObj.updateEntity(rider);
+			rider.mountEntity(entity);
 		}
 
 		return entity;
@@ -62,19 +60,19 @@ public class Teleporter {
 
 	private static Entity transferEntity(Entity entity, double x, double y, double z, WorldServer world) {
 		if (entity.worldObj.provider.dimensionId != world.provider.dimensionId)
-			entity = moveToDimension(entity, world);
-		entity = moveWithinDimension(entity, x, y, z);
-		entity.worldObj.updateEntityWithOptionalForce(entity, false);
+			entity = moveToDimension(entity, x, y, z, world);
+		else
+			moveWithinDimension(entity, x, y, z);
 		return entity;
 	}
 	
-	private static Entity moveToDimension(Entity entity, WorldServer toWorld) {
+	private static Entity moveToDimension(Entity entity, double x, double y, double z, WorldServer toWorld) {
 		return entity instanceof EntityPlayer
-			? movePlayerToDimension((EntityPlayerMP)entity, toWorld)
-			: moveEntityToDimension(entity, toWorld);
+			? movePlayerToDimension((EntityPlayerMP)entity, x, y, z, toWorld)
+			: moveEntityToDimension(entity, x, y, z, toWorld);
 	}
 
-	private static Entity moveEntityToDimension(Entity entity, WorldServer toWorld) {
+	private static Entity moveEntityToDimension(Entity entity, double x, double y, double z, WorldServer toWorld) {
 		NBTTagCompound tag = new NBTTagCompound();
 		entity.writeToNBTOptional(tag);
 		
@@ -87,11 +85,13 @@ public class Teleporter {
 		entity.forceSpawn = true;
 		toWorld.spawnEntityInWorld(entity);
 		entity.setWorld(toWorld);
+		entity.setLocationAndAngles(x, y, z, entity.rotationYaw, entity.rotationPitch);
+		toWorld.updateEntityWithOptionalForce(entity, false);
 		
 		return entity;
 	}
 
-	private static Entity movePlayerToDimension(EntityPlayerMP player, WorldServer toWorld) {
+	private static Entity movePlayerToDimension(EntityPlayerMP player, double x, double y, double z, WorldServer toWorld) {
 		MinecraftServer server = player.mcServer;
 		ServerConfigurationManager config = server.getConfigurationManager();
 		WorldServer fromWorld = (WorldServer) player.worldObj;
@@ -109,40 +109,50 @@ public class Teleporter {
 		// removePlayerEntityDangerously cannot be used here -
 		// it would break loadedEntitiesList and thus unapplicable inside updateEntities
 		fromWorld.removeEntity(player);
+		int chunkX = player.chunkCoordX;
+		int chunkZ = player.chunkCoordZ;
+		
+		if (player.addedToChunk && fromWorld.getChunkProvider().chunkExists(chunkX, chunkZ))
+			fromWorld.getChunkFromChunkCoords(chunkX, chunkZ).removeEntity(player);
+		
 		fromWorld.unloadEntities(Arrays.asList(player));
-		fromWorld.getPlayerManager().removePlayer(player);
+		fromWorld.onEntityRemoved(player);
 		player.isDead = false;
-
-		toWorld.getPlayerManager().addPlayer(player);
+		
 		toWorld.spawnEntityInWorld(player);
+		player.setLocationAndAngles(x, y, z, player.rotationYaw, player.rotationPitch);
+		toWorld.updateEntityWithOptionalForce(player, false);
+		player.setLocationAndAngles(x, y, z, player.rotationYaw, player.rotationPitch);
 		player.setWorld(toWorld);
+		
+		fromWorld.getPlayerManager().removePlayer(player);
+		toWorld.getPlayerManager().addPlayer(player);
+		toWorld.theChunkProviderServer.loadChunk((int)player.posX >> 4, (int)player.posZ >> 4);
+		
+		player.setPositionAndUpdate(x, y, z);
 		player.theItemInWorldManager.setWorld(toWorld);
 
 		config.updateTimeAndWeatherForPlayer(player, toWorld);
 		config.syncPlayerInventory(player);
 
 		for (Object potion: player.getActivePotionEffects())
-			player
-				.playerNetServerHandler
-				.sendPacketToPlayer(
-					new Packet41EntityEffect(player.entityId, (PotionEffect) potion)
-				);
+			player.playerNetServerHandler.sendPacketToPlayer(
+				new Packet41EntityEffect(player.entityId, (PotionEffect) potion)
+			);
 
 		player.playerNetServerHandler
-				.sendPacketToPlayer(new Packet43Experience(
-						player.experience, player.experienceTotal,
-						player.experienceLevel));
+			.sendPacketToPlayer(new Packet43Experience(
+				player.experience, player.experienceTotal,
+				player.experienceLevel));
 		GameRegistry.onPlayerChangedDimension(player);
 
 		return player;
 	}
 	
-	private static Entity moveWithinDimension(Entity entity, double x, double y, double z) {
+	private static void moveWithinDimension(Entity entity, double x, double y, double z) {
 		if (entity instanceof EntityPlayer)
 			((EntityPlayerMP)entity).setPositionAndUpdate(x, y, z);
 		else
 			entity.setLocationAndAngles(x, y, z, entity.rotationYaw, entity.rotationPitch);
-		
-		return entity;
 	}
 }
