546b673c7d461b13ed1ddf20262e9be0c07163f6
target-san <target.san@gmail.com>
2014-05-29 02:32:39 +0300

    Added proper name remapping into class visitor. Added stub for method visitor.

--- src/main/scala/org/targetsan/mcmods/gateway/GatewayMod.scala
+++ src/main/scala/org/targetsan/mcmods/gateway/GatewayMod.scala
@@ -7,6 +7,8 @@ import cpw.mods.fml.relauncher.IFMLLoadingPlugin
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion
 import net.minecraft.launchwrapper.IClassTransformer
 import org.objectweb.asm._
+import cpw.mods.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper
+import org.objectweb.asm.commons.RemappingClassAdapter
 
 @Mod(modid = "gateway", useMetadata = true, modLanguage = "scala")
 object GatewayMod {
@@ -25,7 +27,7 @@ class GatewayCoreMod extends IFMLLoadingPlugin
 {
 	override def getASMTransformerClass: Array[String] = Array("org.targetsan.mcmods.gateway.FlintAndSteelPatcher")
 	override def getModContainerClass: String = null
-	override def getSetupClass: String = null
+	override def getSetupClass: String = null //getClass.getName
 	override def injectData(data: java.util.Map[String, Object]) { }
 	override def getAccessTransformerClass: String = null
 }
@@ -39,10 +41,22 @@ class FlintAndSteelPatcher extends IClassTransformer
 	private def applyPatch(bytes: Array[Byte]): Array[Byte] =
 	{
 		val reader = new ClassReader(bytes)
-		val writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS)
+		val writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS)
+		val patcher = new ClassPatcher(writer)
+		val remapper = new RemappingClassAdapter(patcher, FMLDeobfuscatingRemapper.INSTANCE)
 		
-		reader.accept(writer, 0)
+		reader.accept(remapper, ClassReader.EXPAND_FRAMES)
 		writer.toByteArray()
+	}
+	
+	private class ClassPatcher(cv: ClassVisitor) extends ClassVisitor(Opcodes.ASM4, cv)
+	{
+		override def visitMethod(access: Int, name: String, desc: String, signature: String, exceptions: Array[String]): MethodVisitor =
+		{
+			System.out.println(s"Found method $name $desc") // our target method is SRG func_77648_a == onItemUse
+			
+			super.visitMethod(access, name, desc, signature, exceptions)
+		}
 	}
 }
 
