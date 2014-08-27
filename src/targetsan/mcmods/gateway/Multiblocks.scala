45aa741882997eea04d48372bc005fe582aa52d4
target-san <target.san@gmail.com>
2014-08-27 14:03:17 +0300

    Implemented volume markup algorithm TODO: position rating, sorting, selection

--- src/targetsan/mcmods/gateway/Multiblocks.scala
+++ src/targetsan/mcmods/gateway/Multiblocks.scala
@@ -155,30 +155,82 @@ object RedstoneCoreMultiblock extends MultiblockImpl
 	private object BlockType extends Enumeration
 	{
 		type BlockType = Value
-		val Invalid, Solid, Complex, Liquid, Air = Value
+		val None, Invalid, Solid, Complex, Liquid, Air = Value
 	}
-	// Maps specified volume 
-	private def scanVolume(w: World, cx: Int, cy: Int, cz: Int, deadR: Int, lookupR: Int, lookupH: Int): ((Int, Int, Int) => BlockType.BlockType, (Int, Int, Int) => Boolean) =
+	// Scans volume and returns mapping - coordinates to block type
+	// Algorithm suggests that endpoint volume shouldn't contain 'invalid' blocks
+	// So presense of other gateways is marked by 'Invalid' type, returned in certain radius
+	// Which is 1 less than dead zone - because endpoint radius is 1
+	private val DeadR = 4
+	private val LookupR = 3
+	private val EndpointR = 2
+	private val HeightFractions = 4 // N, used for 1/Nth of actual dimension height
+	
+	private val EffDeadR = DeadR - EndpointR
+	private val EffLookupR = LookupR + EndpointR
+	
+	private def scanVolume(w: World, cx: Int, cy: Int, cz: Int): (Int, Int, Int) => BlockType.BlockType =
 	{
-		val dx = x1 - x0
-		val dy = y1 - y0
-		val dz = z1 - z0
-		
-		val blocks = Utils
-			.enumVolume(x0, y0, z0, x1, y1, z1)
-			.map { case (x, y, z) =>
-				if (w.isAirBlock(x, y, z)) BlockType.Air
-				else if (w.isBlockNormalCubeDefault(x, y, z, false)) BlockType.Solid
-				else if (w.getBlock(x, y, z).isInstanceOf[BlockLiquid]) BlockType.Liquid
-				else BlockType.Complex
-			}
-		
-		return { (x, y, z) =>
-			if (x < x0 || x > x1 || y < y0 || y > y1 || z < z0 || z > z1) BlockType.Invalid
-			else blocks(
-				(z - z0) + dz * ( (y - y0) + dy * (x - x0) )
+		val LookupH = w.provider.getActualHeight() / HeightFractions
+
+		val lookupX = cx - EffLookupR to cx + EffLookupR
+		val lookupY = cy - LookupH to cy + LookupH
+		val lookupZ = cz - EffLookupR to cz + EffLookupR
+		// Dead zone markup manipulation; no need to span across whole zone, as the region outside lookup is dead anyway
+		val deadFieldStorage = Array.fill(lookupX.length * lookupZ.length)(false) // defines dead zone
+		def isDead(x: Int, z: Int): Boolean =
+			if ((lookupX contains x) && (lookupZ contains z))
+				deadFieldStorage((x - lookupX.start) * lookupZ.length + z - lookupZ.start)
+			else true
+
+		def setDead(x: Int, z: Int, value: Boolean): Unit =
+			if ((lookupX contains x) && (lookupZ contains z))
+				deadFieldStorage((x - lookupX.start) * lookupZ.length + z - lookupZ.start) = value
+		// Lookup zone manipulation
+		val typeStorage = Array.fill(lookupX.length * lookupY.length * lookupZ.length)(BlockType.None)
+		def getType(x: Int, y: Int, z: Int): BlockType.BlockType =
+			if (
+				!(lookupX contains x) ||
+				!(lookupY contains y) ||
+				!(lookupZ contains z) ||
+				isDead(x, z)
 			)
+				BlockType.Invalid
+			else typeStorage(
+				((x - lookupX.start) * lookupY.length + (y - lookupY.start )) * lookupZ.length + (z - lookupZ.start)
+			)
+		def setType(x: Int, y: Int, z: Int, t: BlockType.BlockType): Unit =
+			if ( (lookupX contains x) &&
+				 (lookupY contains y) &&
+				 (lookupZ contains z)
+			)
+				typeStorage(
+						((x - lookupX.start) * lookupY.length + (y - lookupY.start )) * lookupZ.length + (z - lookupZ.start)
+				) = t
+		// Perform full zone scan
+		val FullR = EffLookupR + EffDeadR
+		for ( (x, y, z) <- Utils.enumVolume(cx - FullR, 1, cz - FullR, cx + FullR, w.provider.getActualHeight() - 1, cz + FullR))
+		{
+			val entity = w.getTileEntity(x, y, z)
+			// Mark all columns in other GW's 'dead zone' as 'dead'
+			if (entity != null && entity.isInstanceOf[TileGateway])
+				for {x1 <- x - EffDeadR to x + EffDeadR; z1 <- z - EffDeadR to z + EffDeadR}
+					setDead(x, y, true)
+			// Qualify block only if it wasn't already qualified, or shadowed by dead zone
+			if (getType(x, y, z) == BlockType.None)
+				setType(x, y, z,
+					{
+						val b = w.getBlock(x, y, z)
+						if (b.getBlockHardness(w, x, y, z) < 0) BlockType.Invalid
+						else if (b.isAir(w, x, y, z)) BlockType.Air
+						else if (b.isInstanceOf[BlockLiquid]) BlockType.Liquid 
+						else if (b.isBlockNormalCube()) BlockType.Solid
+						else BlockType.Complex
+					}
+				)
 		}
+		// Return getter func as-is
+		getType
 	}
 }
 
