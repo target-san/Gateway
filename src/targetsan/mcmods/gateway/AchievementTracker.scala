4b309679e69b7521d0e3bbefa63f867c8a5bbfbf
target-san <target.san@gmail.com>
2014-11-02 11:31:06 +0200

    Fixed achievement trigger - now all achievements are triggered properly

--- src/targetsan/mcmods/gateway/AchievementTracker.scala
+++ src/targetsan/mcmods/gateway/AchievementTracker.scala
@@ -9,11 +9,13 @@ import targetsan.mcmods.gateway.Utils._
 object AchievementTracker {
 	@SubscribeEvent
 	def onPlayerTravel(event: api.GatewayTravelEvent.Leave): Unit =
-		for (player <- event.entity.as[EntityPlayer])
-			(event.fromWorld.provider.dimensionId, event.toWorld.provider.dimensionId) match {
-				case (_, NetherDimensionId) => player.triggerAchievement(AchievementList.portal)
-				case (_, EndDimensionId) => player.triggerAchievement(AchievementList.theEnd)
-				case (EndDimensionId, _) => player.triggerAchievement(AchievementList.theEnd2)
-				case _ => ()
-			}
+		for (player <- event.entity.as[EntityPlayer]) {
+			val fromId = event.fromWorld.provider.dimensionId
+			val toId = event.toWorld.provider.dimensionId
+			val trigger = player.triggerAchievement _
+
+			if (toId == NetherDimensionId) trigger(AchievementList.portal)
+			if (toId == EndDimensionId) trigger(AchievementList.theEnd)
+			if (fromId == EndDimensionId) trigger(AchievementList.theEnd2)
+		}
 }
